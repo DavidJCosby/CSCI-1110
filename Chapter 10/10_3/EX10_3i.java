b168ce47db2e2bc608d1f766c843cfa988a8bce4
David Cosby <60022092@ironschools.org>
2019-11-14 09:42:18 -0700

    completed 10_3, split into 2 files

--- Chapter 10/10_3/EX10_3i.java
+++ Chapter 10/10_3/EX10_3i.java
@@ -2,93 +2,38 @@
 Author: David Cosby
 Date: 11/12/2019
 
-Code description goes here
+Tests each of the methods in the MyInteger class
 */
-
-class MyInteger {
-	public int value;
-	
-	MyInteger(int v) {
-		value = v;
-	}
-	
-	public int getValue() {
-		return value;
-	}
-	
-	public static boolean isEven(int v) {
-		return (v%2 == 0);
-	}
-	
-	public static boolean isOdd(int v) {
-		return (v%2 == 1);
-	}
-	
-	public static boolean isPrime(int v) {
-		if (v == 0 || v == 1) {
-			return false;
-		}
-		else {
-			for (int i = 2; i < v; i++) {
-				if (v%i == 0) {
-					System.out.println(v + " yee " + i);
-					return false;
-				}
-			}
-			return true;
-		}
-	}
-	
-	public static boolean isEven(MyInteger v) {
-		return MyInteger.isEven(v.getValue());
-	}
-		
-	public static boolean isOdd(MyInteger v) {
-		return MyInteger.isOdd(v.getValue());
-	}
-		
-	public static boolean isPrime(MyInteger v) {
-		return MyInteger.isPrime(v.getValue());
-	}
-	
-	public boolean isEven() {
-		return MyInteger.isEven(value);
-	}
-	
-	public boolean isOdd() {
-		return MyInteger.isOdd(value);
-	}
-	
-	public boolean isPrime() {
-		return MyInteger.isPrime(value);
-	}
-	
-	public boolean equals(int v) {
-		return (v == value);
-	}
-	
-	public boolean equals(MyInteger v) {
-		return equals(v.getValue());
-	}
-	
-	public static int parseInt(String str) {
-		return Integer.parseInt(str);
-	}
-	
-	public static int parseInt(char[] chars) {
-		String s = "";
-		for (char c : chars) {
-			s += c;
-		}
-		return MyInteger.parseInt(s);
-	}
-	
-
-}
-
 
 class EX10_3i {
 	public static void main(String[] args) {
-		MyInteger.parseInt(char[] chars)
+		MyInteger x = new MyInteger(53);
+		MyInteger z = new MyInteger(11);
+		
+		System.out.println("X: " + x.getValue());
+		System.out.println("is even: " + x.isEven());
+		System.out.println("is odd: " + x.isOdd());
+		System.out.println("is prime: " + x.isPrime());
+		
+		System.out.println("\nY: 64");
+		System.out.println("is even: " + MyInteger.isEven(64));
+		System.out.println("is odd: " + MyInteger.isOdd(64));
+		System.out.println("is prime: " + MyInteger.isPrime(64));
+		
+		System.out.println("\nZ: " + z.getValue());
+		System.out.println("is even: " + MyInteger.isEven(z));
+		System.out.println("is odd: " + MyInteger.isOdd(z));
+		System.out.println("is prime: " + MyInteger.isPrime(z));
+		
+		System.out.println("\nX == 53: " + x.equals(53));
+		System.out.println("X == Z: " + x.equals(z));
+		
+		System.out.println("\nCasting characters '5', '2,' and '3' to an integer.");
+		char[] chars = {'5', '2', '3'};
+		System.out.println(MyInteger.parseInt(chars));
+		
+		System.out.println("\nCasting string \"98765\" to an integer.");
+		System.out.println(MyInteger.parseInt("98765"));
+
 	}
 }
---
+++ Chapter 10/10_3/MyInteger.java
@@ -0,0 +1,86 @@
+/*
+Author: David Cosby
+Date: 11/12/2019
+
+Custom "MyInteger" class with various static and non-static methods.
+*/
+
+class MyInteger {
+	public int value;
+	
+	MyInteger(int v) {
+		value = v;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public static boolean isEven(int v) {
+		return (v%2 == 0);
+	}
+	
+	public static boolean isOdd(int v) {
+		return (v%2 == 1);
+	}
+	
+	public static boolean isPrime(int v) {
+		if (v == 0 || v == 1) {
+			return false;
+		}
+		else {
+			for (int i = 2; i < v; i++) {
+				if (v%i == 0) {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+	
+	public static boolean isEven(MyInteger v) {
+		return MyInteger.isEven(v.getValue());
+	}
+		
+	public static boolean isOdd(MyInteger v) {
+		return MyInteger.isOdd(v.getValue());
+	}
+		
+	public static boolean isPrime(MyInteger v) {
+		return MyInteger.isPrime(v.getValue());
+	}
+	
+	public boolean isEven() {
+		return MyInteger.isEven(value);
+	}
+	
+	public boolean isOdd() {
+		return MyInteger.isOdd(value);
+	}
+	
+	public boolean isPrime() {
+		return MyInteger.isPrime(value);
+	}
+	
+	public boolean equals(int v) {
+		return (v == value);
+	}
+	
+	public boolean equals(MyInteger v) {
+		return equals(v.getValue());
+	}
+	
+	public static int parseInt(String str) {
+		return Integer.parseInt(str);
+	}
+	
+	public static int parseInt(char[] chars) {
+		String s = "";
+		for (char c : chars) {
+			s += c;
+		}
+		return MyInteger.parseInt(s);
+	}
+	
+
+}
