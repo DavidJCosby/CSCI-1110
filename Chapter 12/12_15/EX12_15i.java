e708e97d50fa2b012bcbc3baeaafb83604428391
David Cosby <60022092@ironschools.org>
2019-11-26 10:30:00 -0700

    Started EX12_15i.java

---
+++ Chapter 12/12_15/EX12_15i.java
@@ -0,0 +1,12 @@
+/*
+Author: David Cosby
+Date: 11/26/2001
+
+If Exercise12_15.txt doesn't exist, it will write 100 random integers to it. Another method will print the integers in Exercise12_15.txt after sorting them in increasing order.
+*/
+
+class EX12_15i {
+	public static void main(String[] args) {
+		
+	}
+}
