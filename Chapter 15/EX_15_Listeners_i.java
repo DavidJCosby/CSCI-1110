6d1b45632e150e23486d780c0be45db1902ba036
404kevnotfound <student@HSiMac4-13.local>
2020-01-14 10:05:22 -0700

    Finished EX_15_Listeners_i.java

--- Chapter 15/EX_15_Listeners_i.java
+++ Chapter 15/EX_15_Listeners_i.java
@@ -8,12 +8,15 @@ Creates a BallPane and binds keyboard inputs to its movement methods.
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 
 
 public class EX_15_Listeners_i extends Application {
 	
-	public BallPane setUpBall() {
+	public BallPane setupBall() {
 		BallPane ball = new BallPane(300, 300, 50);		
 		ball.setOnKeyPressed( e -> {
 			if (e.getCode() == KeyCode.UP) {
@@ -33,19 +36,57 @@ public class EX_15_Listeners_i extends Application {
 		return ball;
 	}
 	
+	public VBox setupButtonsAndBindTo(BallPane ball) {
+		VBox container = new VBox();
+		container.setAlignment(Pos.TOP_RIGHT);
+		
+		Button upButton = new Button("Up");
+		upButton.setFocusTraversable(false);
+		upButton.setOnAction(e -> {
+			ball.moveUp();
+		});
+		
+		Button leftButton = new Button("Left");
+		leftButton.setFocusTraversable(false);
+		leftButton.setOnAction(e -> {
+			ball.moveLeft();
+		});
+		
+		Button rightButton = new Button("Right");
+		rightButton.setFocusTraversable(false);
+		rightButton.setOnAction(e -> {
+			ball.moveRight();
+		});
+		
+		Button downButton = new Button("Down");
+		downButton.setFocusTraversable(false);
+		downButton.setOnAction(e -> {
+			ball.moveDown();
+		});
+		
+		container.getChildren().addAll(upButton, leftButton, rightButton, downButton);
+		return container;
+	}
+	
 	public void start(Stage primaryStage) {
-		BallPane ball = setUpBall();
+		BallPane ball = setupBall();
+		VBox buttons = setupButtonsAndBindTo(ball);
+		BorderPane root = new BorderPane();
 		
+		root.setCenter(ball);
+		root.setRight(buttons);
 		
-		Scene scene = new Scene(ball, 600, 600);
+		Scene scene = new Scene(root, 600, 600);
 		primaryStage.setScene(scene);
-		primaryStage.setTitle("Ye");
+		primaryStage.setResizable(false);
+		primaryStage.setTitle("Ball bound to keyboard and buttons");
 		primaryStage.show();
+		
+		ball.setScreenWidth(ball.getWidth());
+		ball.setScreenHeight(ball.getHeight());
 		
 		ball.requestFocus();
 	}
-
-
 }
 
 
